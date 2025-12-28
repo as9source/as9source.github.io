e51013e9165a59f76a16233b639b482b5eba4cd6
as9source <as9css@gmail.com>
2025-12-28 11:09:36 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,35 @@
+// Service Workerを自動削除
+// このファイルは自分自身を削除して、キャッシュ問題を解決します
+
+self.addEventListener('install', function(e) {
+  // すぐにアクティブ化
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    // Service Workerを登録解除
+    self.registration.unregister()
+      .then(function() {
+        // すべてのキャッシュを削除
+        return caches.keys().then(function(cacheNames) {
+          return Promise.all(
+            cacheNames.map(function(cacheName) {
+              return caches.delete(cacheName);
+            })
+          );
+        });
+      })
+      .then(function() {
+        // すべてのクライアント（タブ）を取得
+        return self.clients.matchAll();
+      })
+      .then(function(clients) {
+        // すべてのタブをリロード
+        clients.forEach(function(client) {
+          client.navigate(client.url);
+        });
+      })
+  );
+});
 
