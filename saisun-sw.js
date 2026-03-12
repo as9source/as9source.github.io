b1fa905039ca883714cc9153620bf6a1605e6f45
as9source <as9css@gmail.com>
2026-03-12 21:50:55 +0900

    Add files via upload

---
+++ saisun-sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'saisun-v1';
+const ASSETS = [
+  '/saisun.html',
+  '/saisun-manifest.json',
+  '/saisun-icon.png',
+  '/saisun-icon-192.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
